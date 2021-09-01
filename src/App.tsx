e0e21365deaf114e0ef5da9f59c358c102bcab64
peter <dklirser@gmail.com>
2021-09-01 17:37:03 +0900

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import styled from "styled-components";
-
 import { Provider } from "react-redux";
 import store from "./redux/index";
 
-import { Route } from "react-router-dom";
-
-import Login from "./components/Login";
-import Detail from "./components/Detail";
-import Main from "./components/Main";
 import Background from "./components/Background";
 
 function App() {
-  // const [user, loginState] = useSelector<GlobalState, [UserAccount, boolean]>(
-  //   (state) => [state.userAccount, state.loginState]
-  // );
   return (
     <Provider store={store}>
       <Background />
